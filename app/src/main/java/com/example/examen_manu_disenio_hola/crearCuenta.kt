050e78ab6788b1515e5c772692f82ec2f83df27a
Jimmyh091 <jaimecaballero164@gmail.com>
2024-12-11 17:29:54 +0100

    telminado??????

--- app/src/main/java/com/example/examen_manu_disenio_hola/crearCuenta.kt
+++ app/src/main/java/com/example/examen_manu_disenio_hola/crearCuenta.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -59,6 +60,9 @@ class crearCuenta : AppCompatActivity() {
                 intentguardar.putExtra("nombre", tietnombre.text.toString())
                 intentguardar.putExtra("pass", tietpass.text.toString())
                 intentguardar.putExtra("correo", tietcorreo.text.toString())
+
+                val toastcrear = Toast.makeText(this, "Usuario creado", Toast.LENGTH_SHORT).show()
+
                 startActivity(intentguardar)
             }else{
                 if (!nombre) tietnombre.error = "Nombre muy largo"
